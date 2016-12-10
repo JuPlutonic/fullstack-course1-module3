753b20c520a797d7378c26042a5aaed740d6cab5
0x639d1dec74 <guhtymailru@mailinator.com>
2016-12-10 15:26:00 +0100

    Implements .for

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -3,7 +3,11 @@ require 'pp'
 
 class Recipe
  include HTTParty
- base_uri "http://food2fork.com/api/search"
- default_Params key: ENV["c2b5d0ebe1a8da039c568bbf7a2153da"]
+ base_uri "http://food2fork.com/api"
+ default_params key: ENV["c2b5d0ebe1a8da039c568bbf7a2153da"]
  format :json
+
+ def self.for term
+  get("/search/", query: {q: term})["elements"]
+ end
 end
