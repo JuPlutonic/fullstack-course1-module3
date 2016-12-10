9240b222008c8d5a35a05a9ea6fd23a164130e9f
0x639d1dec74 <guhtymailru@mailinator.com>
2016-12-10 15:07:36 +0100

    Added API |<3`/ but in the future in must be placed in another file and git|(;|\|0|23|)

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,9 @@
 require 'httparty'
+require 'pp'
 
 class Recipe
-
+ include HTTParty
+ base_uri "http://food2fork.com/api/search"
+ default_Params key: ENV["c2b5d0ebe1a8da039c568bbf7a2153da"]
+ format :json
 end
